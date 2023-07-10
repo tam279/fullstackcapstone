10683c57dcb4c147ab19984b03d0fd1ffe3def11
Tran Nguyen <nguyentc.cmu@vnpt.vn>
2023-07-11 06:59:35 +0700

    refactor config

--- server/db/database.js
+++ server/db/database.js
@@ -1,12 +1,13 @@
 // database.js . Path: C:\Users\nguye\OneDrive\Desktop\project-managment-website\server\db\database.js
 
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'password',
-    database: process.env.DB_NAME || 'pmsdatabase',
+    host: process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE_NAME || 'pmsdatabase',
 });
 
 
