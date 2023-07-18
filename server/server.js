acea5c861c46798ac3a1f37e6c0d208929a735c3
azriee <dfntlyaz@gmail.com>
2023-07-18 10:07:14 -0600

    Cors Update

--- server/server.js
+++ server/server.js
@@ -36,6 +36,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://capdep-1vfm28xyt-azriee.vercel.app",
+      "https://capdep.vercel.app/",
     ], // replace this with your React application URL
     credentials: true, // this enables cookies to be sent with requests from the client
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"], // add 'OPTIONS' to this array
